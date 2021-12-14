4a275675f29a4c0ec99788b0f65df271c6650925
tral909 <indorm1992@mail.ru>
2021-12-14 09:46:56 +0300

    Implement getBlogPost(s)

--- src/main/kotlin/io/tral909/BlogService.kt
+++ src/main/kotlin/io/tral909/BlogService.kt
@@ -1,9 +1,10 @@
 package io.tral909
 
 import com.linecorp.armeria.common.HttpResponse
-import com.linecorp.armeria.server.annotation.Post
-import com.linecorp.armeria.server.annotation.RequestConverter
+import com.linecorp.armeria.server.annotation.*
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.NoSuchElementException
+
 
 class BlogService {
     private val blogPosts: MutableMap<Int, BlogPost> = ConcurrentHashMap()
@@ -13,5 +14,23 @@ class BlogService {
     fun createBlogPost(blogPost: BlogPost): HttpResponse {
         blogPosts[blogPost.id] = blogPost
         return HttpResponse.ofJson(blogPost)
+    }
+
+    @Get("/blogs/:id")
+    fun getBlogPost(@Param("id") id: Int): HttpResponse =
+        HttpResponse.ofJson(blogPosts[id] ?: throw NoSuchElementException("Post with id = $id is not found!"))
+
+    @Get("/blogs")
+    @ProducesJson
+    fun getBlogPosts(@Param("desc") @Default("true") descending: Boolean): Iterable<BlogPost> {
+        // Descending
+        if (descending) {
+            return blogPosts.entries
+                .sortedByDescending { it.key }
+                .map { it.value }
+                .toCollection(mutableListOf())
+        }
+        // Ascending
+        return blogPosts.values
     }
 }
